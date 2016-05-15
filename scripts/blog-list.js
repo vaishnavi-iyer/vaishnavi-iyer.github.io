ad1dfd33e8bd31ac9b0fd4d7f5ef183627940693
vaishnavi-iyer <iyervaishnavi26@gmail.com>
2016-05-15 22:21:42 +1200

    adding t6 and c6 blogs

--- scripts/blog-list.js
+++ scripts/blog-list.js
@@ -1,75 +1,9 @@
-<section class="blog-list"> <h4> Blogs</h4>
-  <div class="value-props row">
-    <div class="four columns value-prop">
-      <a href="c1-reflection-blog.html""Reflections">
-        <img class="value-img" src="http://placehold.it/100x100">
-        Reflections 
-      </a>
-    </div>
-    <div class="four columns value-prop">
-      <a href="c1-time-and-habits-blog.html""Time &amp; Habits">
-        <img class="value-img" src="http://placehold.it/100x100">
-        Time &amp; Habits </a>
-    </div>
-    <div class="four columns value-prop">
-      <a href="c2-emotional-intelligence.html""Emotional Intellingence">
-        <img class="value-img" src="http://placehold.it/100x100">
-        Emotional Intelligence 
-      </a>
-    </div>
-  </div>
-  <div class="value-props row">
-    <div class="four columns value-prop">
-      <a href="c3-meditation-process.html""Meditation Process">
-        <img class="value-img" src="http://placehold.it/100x100">
-        Meditation Process
-      </a>
-    </div>
-    <div class="four columns value-prop">
-      <a href="t2-html-css-dom-p1.html""HTML CSS &amp; DOM -1">
-        <img class="value-img" src="http://placehold.it/100x100">
-        HTML CSS &amp; DOM -1
-      </a>
-    </div>
-    <div class="four columns value-prop">
-      <a href="t2-html-css-dom-p2.html""HTML CSS &amp; DOM -2">
-        <img class="value-img" src="http://placehold.it/100x100">
-        HTML CSS &amp; DOM -2
-      </a>
-    </div>
-  </div>
-  <div class="value-props row">
-    <div class="four columns value-prop">
-      <a href="t3-design-to-web-blog.html""design to web">
-        <img class="value-img" src="http://placehold.it/100x100">
-        Design to web
-      </a>
-    </div>
-    <div class="four columns value-prop">
-      <a href="t4-javascript-basics.html""Javascript Basics">
-        <img class="value-img" src="http://placehold.it/100x100">
-        JavaScript Basics
-      </a>
-    </div>
-    <div class="four columns value-prop">
-      <a href="c4-daily-mindfulness.html""Daily Mindfulness">
-        <img class="value-img" src="http://placehold.it/100x100">
-        Daily Mindfulness
-      </a>
-    </div>
-  </div>
-  <div class="value-props row">
-    <div class="four columns value-prop">
-      <a href="c5-self-confidence.html""Self Confidence">
-        <img class="value-img" src="http://placehold.it/100x100">
-        Self Confidence
-      </a>
-    </div>
-    <div class="four columns value-prop">
-      <a href="t5-problem-solving.html""Problem Solving">
-        <img class="value-img" src="http://placehold.it/100x100">
-        Problem Solving
-      </a>
-    </div>
-  </div>
-</section>
+
+var blogList = '<h4> Blogs</h4>';
+var row1 = '<div class="value-props row"> <div class="four columns value-prop"> <a href="c1-reflection-blog.html""Reflections"> <img class="value-img" src="http://placehold.it/100x100"> Reflections </a></div><div class="four columns value-prop"><a href="c1-time-and-habits-blog.html""Time &amp; Habits"><img class="value-img" src="http://placehold.it/100x100">Time &amp; Habits </a></div><div class="four columns value-prop"><a href="c2-emotional-intelligence.html""Emotional Intellingence"><img class="value-img" src="http://placehold.it/100x100">Emotional Intelligence </a></div></div>'
+var row2 = '<div class="value-props row"> <div class="four columns value-prop"> <a href="c3-meditation-process.html""Meditation Process"> <img class="value-img" src="http://placehold.it/100x100"> Meditation Process </a> </div> <div class="four columns value-prop"> <a href="t2-html-css-dom-p1.html""HTML CSS &amp; DOM -1"> <img class="value-img" src="http://placehold.it/100x100"> HTML CSS &amp; DOM -1 </a> </div> <div class="four columns value-prop"> <a href="t2-html-css-dom-p2.html""HTML CSS &amp; DOM -2"> <img class="value-img" src="http://placehold.it/100x100"> HTML CSS &amp; DOM -2 </a> </div> </div>'
+var row3 = '<div class="value-props row"> <div class="four columns value-prop"> <a href="t3-design-to-web-blog.html""design to web"> <img class="value-img" src="http://placehold.it/100x100"> Design to web </a> </div> <div class="four columns value-prop"> <a href="t4-javascript-basics.html""Javascript Basics"> <img class="value-img" src="http://placehold.it/100x100"> JavaScript Basics </a> </div> <div class="four columns value-prop"> <a href="c4-daily-mindfulness.html""Daily Mindfulness"> <img class="value-img" src="http://placehold.it/100x100"> Daily Mindfulness </a> </div> </div>'
+var row4 = '<div class="value-props row"> <div class="four columns value-prop"> <a href="c5-self-confidence.html""Self Confidence"> <img class="value-img" src="http://placehold.it/100x100"> Self Confidence </a> </div> <div class="four columns value-prop"> <a href="t5-problem-solving.html""Problem Solving"> <img class="value-img" src="http://placehold.it/100x100"> Problem Solving </a> </div> <div class="four columns value-prop"> <a href="c6-control.html""Control"> <img class="value-img" src="http://placehold.it/100x100"> Self Regulation </a> </div> </div>'
+var row5 = '<div class="value-props row"> <div class="four columns value-prop"> <a href="t6-js-language.html""JavaScript"> <img class="value-img" src="http://placehold.it/100x100"> Javascript Language </a> </div> </div>'
+
+document.getElementById('blogList').innerHTML += blogList + row1 + row2 + row3 + row4 + row5;
